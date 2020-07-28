33014eef5baecd5cc12d6f135fdef18d7025c821
jessie <245868553@qq.com>
2020-07-28 18:21:14 +0800

    根系

--- flutter_module/lib/main.dart
+++ flutter_module/lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
+      initialRoute:"/",
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
